6f2221d8c700f2bc26b7ba9936dfa06225e1f532
Anastasia Xin <yeeching.hsin@gmail.com>
2023-03-13 15:23:05 -0400

    printCell

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -13,11 +13,12 @@ function contains(cell) {
 }
 
 const printCell = (cell, state) => {
-  if (contains.call(state, cell)) {
-    return `\u25A3`;
-  } else {
-    return `\u25A2`;
-  }
+  // if (contains.call(state, cell)) {
+  //   return `\u25A3`;
+  // } else {
+  //   return `\u25A2`;
+  // }
+  return contains.call(this, cell) ? `\u25A3` : `\u25A2`;
 };
 
 const corners = (state = []) => {};
